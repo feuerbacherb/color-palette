db9707060d76be8ad921cd5eebc76ee4112e5026
Brent Feuerbacher <feuerbacherb@gmail.com>
2020-11-23 08:54:47 -0600

    added NavBar component and styled title page, modified index.html to include Google Roboto font, moved Slider component to NavBar

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 import './Palette.css';
+import NavBar from './NavBar';
 
 class Palette extends Component {
 	constructor(props) {
@@ -20,9 +19,7 @@ class Palette extends Component {
 		const colorBoxes = colors[level].map((color) => <ColorBox background={color.hex} name={color.name} />);
 		return (
 			<div className='Palette'>
-				<div className='slider'>
-					<Slider defaultValue={level} min={100} max={900} onAfterChange={this.changeLevel} step={100} />
-				</div>
+				<NavBar level={level} changeLevel={this.changeLevel} />
 				{/* NavBar goes here */}
 				<div className='Palette-colors'>{colorBoxes}</div>
 				{/* footer */}
