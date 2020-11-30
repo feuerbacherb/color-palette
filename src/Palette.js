e71e7b64b0b7ad7ed8c1833f8c2b3d24b9fa66ec
Brent Feuerbacher <feuerbacherb@gmail.com>
2020-11-30 09:58:24 -0600

    added footer to Palette component, fixed ColorBox key, styled footer

--- src/Palette.js
+++ src/Palette.js
@@ -20,15 +20,20 @@ class Palette extends Component {
 	}
 
 	render() {
-		const { colors } = this.props.palette;
+		const { colors, paletteName, emoji } = this.props.palette;
 		const { level, format } = this.state;
-		const colorBoxes = colors[level].map((color) => <ColorBox background={color[format]} name={color.name} />);
+		const colorBoxes = colors[level].map((color) => (
+			<ColorBox background={color[format]} name={color.name} key={color.id} />
+		));
 		return (
 			<div className='Palette'>
 				<NavBar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
-				{/* NavBar goes here */}
+
 				<div className='Palette-colors'>{colorBoxes}</div>
-				{/* footer */}
+				<footer className='Palette-footer'>
+					{paletteName}
+					<span className='emoji'>{emoji}</span>
+				</footer>
 			</div>
 		);
 	}
