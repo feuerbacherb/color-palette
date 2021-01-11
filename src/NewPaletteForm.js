7c90e401b2fcd8485e2adcf6d707cab25c79c043
Brent Feuerbacher <feuerbacherb@gmail.com>
2021-01-11 12:23:57 -0600

    added ChromePicker, a title, and buttons to the drawer on NewPaletteForm

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -5,14 +5,16 @@ import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 //import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { ChromePicker } from 'react-color';
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const styles = (theme) => ({
 	root         : {
@@ -133,8 +135,24 @@ class NewPaletteForm extends Component {
 						<IconButton onClick={this.handleDrawerClose}>
 							{<ChevronLeftIcon />}
 						</IconButton>
-					</div>
+					</div>{' '}
 					<Divider />
+					<Typography variant='h4'>Design Your Palette</Typography>
+					<div>
+						<Button variant='contained' color='secondary'>
+							Clear Palette
+						</Button>
+						<Button variant='contained' color='primary'>
+							Random Color
+						</Button>
+					</div>
+					<ChromePicker
+						color='purple'
+						onChangeComplete={(newColor) => console.log(newColor)}
+					/>
+					<Button variant='contained' color='primary'>
+						Add Color
+					</Button>
 				</Drawer>
 				<main
 					className={classNames(classes.content, {
